8031bcf0d7550501929c09a27c30bfc2089bce31
Ryoni <carpinus0@gmail.com>
2020-09-09 14:03:09 +0900

    Add Script

---
+++ notionToGithubPage.py
@@ -0,0 +1,88 @@
+import os
+import zipfile
+import shutil
+from datetime import date
+
+
+def ExportFilePath(_line):
+    splitedLine=_line.split('/')
+    path= splitedLine[0].replace('![','')
+    return path
+
+def UnZipFile():
+    findZipFile=False
+    
+    for f in os.listdir():
+        if f.endswith('.zip'):
+            findZipFile=True
+            print("unzip apks file")
+            with zipfile.ZipFile(f) as notionZipFile:
+                notionZipFile.extractall()
+            os.remove(f)
+            break
+    return findZipFile
+
+def FindMarkdownFile():
+    notionMarkDownFile=''
+    for f in os.listdir():
+        if f.endswith('.md'):
+            notionMarkDownFile=f
+            break
+    return notionMarkDownFile
+
+
+def ModifiedMarkDownFile():
+
+    #Input Information
+    fileName=input("Title=")
+    year=int(input("Year="))
+    month=int(input("Month="))
+    day=int(input("Day="))
+
+    currentTimeStr=date(year, month, day).isoformat()
+
+    
+    #Upzip file
+    if UnZipFile()==False:
+        print("Could not find ZipFile")
+        return
+    
+    #Front Matter
+    with open('customFrontMatter.txt','r') as f:
+        customFrontMatter=f.read()
+
+    frontMatter='---\ntitle: "{}"\ndate: {} 00:00:00 +0900\n{}\n---\n'.format(fileName,currentTimeStr,customFrontMatter)
+
+    #Read Notion Markdown
+    notionMarkDownFile=FindMarkdownFile()
+    notionMarkDownFolder=notionMarkDownFile.replace('.md','')
+
+    with open(notionMarkDownFile,'r') as f:
+        
+        n= f.read()
+        lines=n.split('\n')
+        path=''
+        for line in lines:
+            if line.startswith('!['):
+                path=ExportFilePath(line)
+                break
+        n=n.replace(path,'/assets/images/posts/{}'.format(fileName))
+    
+
+    #Write Modified MarkDown
+    newMarkdownFileName="{}-{}.md".format(currentTimeStr,fileName)
+    with open(newMarkdownFileName,'w') as f:
+        f.write(frontMatter+n)
+    
+    #Remove md file
+    os.remove(notionMarkDownFile)
+
+    #Move Resouces file and Remove Folder
+    print("SRC:"+os.curdir+'/{}'.format(notionMarkDownFolder))
+    print("DES:"+os.curdir+'/assets/images/posts/{}'.format(fileName))
+    shutil.move(os.curdir+'/{}'.format(notionMarkDownFolder),os.curdir+'/assets/images/posts/{}'.format(fileName))
+   
+
+
+if __name__ == '__main__':
+    ModifiedMarkDownFile()
